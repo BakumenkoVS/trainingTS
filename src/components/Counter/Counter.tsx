58a0651533cc05a6a15da27262b55a741296a7c6
vlad bakumenko <tinyyy11@yandex.ru>
2022-09-19 15:04:03 +0300

    add counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -2,6 +2,15 @@ import React, { FC } from "react";
 import { CounterProps } from "./types";
 import { styles } from "./Counter.styles";
 
-const Counter: FC<CounterProps> = () => <div>Counter</div>;
+const Counter: FC<CounterProps> = ({title, subTitle, value}) => (
+   <section>
+      <div css={styles.box}>
+         <h2>{title}</h2>
+         <button css={styles.button}>-</button>
+         <h3>{value}</h3>
+         <button css={styles.button}>+</button>
+      </div>
+   </section>
+);
 
 export default Counter;
--- src/components/Counter/types.ts
+++ src/components/Counter/types.ts
@@ -1,1 +1,8 @@
-export interface CounterProps {}
+export interface CounterProps {
+   title: string;
+   subTitle: string;
+   value: number;
+   onChange: (value: number) => void;
+   min?: number;
+   max?: number;
+}
